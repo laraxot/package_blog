a4a05194e76cc7e56785998528d94b9339b155b4
marco76tv <marco.sottana@gmail.com>
2018-11-08 17:10:23 +0100

    fix namespaces

--- src/Controllers/Admin/Blog/Container0/Container1/Container2Controller.php
+++ src/Controllers/Admin/Blog/Container0/Container1/Container2Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace XRA\Blog\Controllers\Container0\Container1;
+namespace XRA\Blog\Controllers\Admin\Blog\Container0\Container1;
 
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Validation\ValidatesRequests;
