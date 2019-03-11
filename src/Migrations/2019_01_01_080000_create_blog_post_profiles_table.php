fb7223515741009cf6d2f9c9df7f7a11f048321b
marco xot <marco.sottana@gmail.com>
2019-03-11 21:00:05 +0100

    .

--- src/Migrations/2019_01_01_080000_create_blog_post_profiles_table.php
+++ src/Migrations/2019_01_01_080000_create_blog_post_profiles_table.php
@@ -1,35 +1,40 @@
 <?php
-
-
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use XRA\Food\Models\Profile as MyModel;  //blog o food ?
+
 class CreateBlogPostProfilesTable extends Migration
 {
-    protected $table = 'blog_post_profiles';
+    //protected $table = 'blog_post_profiles';
+    public function getTable()
+    {
+        return with(new MyModel())->getTable();
+    }
+
 
     public function up()
     {
-        if (!Schema::hasTable($this->table)) {
-            Schema::create($this->table, function (Blueprint $table) {
-                $table->integer('post_id');
-                $table->index('post_id');
+        if (!Schema::hasTable($this->getTable())) {
+            Schema::create($this->getTable(), function (Blueprint $table) {
+                $table->increments('post_id')->primary();
                 //$table->string('article_type',50)->nullable();
                 //$table->datetime('published_at')->nullable();
                 $table->text('bio')->nullable();
                 $table->timestamps();
             });
         }
-        Schema::table($this->table, function (Blueprint $table) {
-            $table->increments('post_id')->change();
+        Schema::table($this->getTable(), function (Blueprint $table) {
+            //$table->increments('post_id')->change();
             //->autoIncrement()
+            $sql='ALTER TABLE '.$this->getTable().' CHANGE COLUMN post_id post_id INT(16) NOT NULL AUTO_INCREMENT FIRST;';
+            \DB::unprepared($sql);
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists($this->table);
+        Schema::dropIfExists($this->getTable());
     }
 }
