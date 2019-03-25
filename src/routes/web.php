bc8b86e1d1a42c7c7a99dbc3174709b933e6ec34
marco xot <marco.sottana@gmail.com>
2019-03-25 21:29:00 +0100

    .

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,10 +12,12 @@ $middleware = ['web', 'guest']; //guest ti riindirizza se non sei loggato
 $middleware = ['web'];
 
 $prefix = 'blog';
+/*
 Route::get('/home', function () {
     return redirect(\App::getLocale());
 });
-
+*/
+/*
 Route::get('redirect', function () {
     $data = \Request::all();
     if (isset($data['url'])) {
@@ -24,6 +26,7 @@ Route::get('redirect', function () {
         return;
     }
 });
+*/
 
 $acts = [
     ['name' => 'attach'], //end act_n
@@ -156,6 +159,8 @@ Route::group([
 ///---- without this route / go to 404
 Route::group(['prefix' => null, 'middleware' => $middleware, 'namespace' => $namespace], function () {
     Route::get('/', 'HomeController@index');
+    Route::get('/redirect', 'HomeController@redirect');
+    Route::get('/test01', 'HomeController@test01');
 });
 
 $this->routes();
@@ -166,10 +171,17 @@ Route::fallback(function() {
 });
 
 //*/
-
+/*
  Route::get('/morph',function () {
     $risto=\XRA\Food\Models\Restaurant::find(14677);
     //ddd($risto->post_id);
     //ddd($risto->post()->toSql());
     ddd($risto->cuisineCats);
 });
+
+ 
+Route::get('/test01',function () {
+    // var_dump(xdebug_get_function_stack());
+    xdebug_print_function_stack( 'Your own message' );
+}); 
+*/
