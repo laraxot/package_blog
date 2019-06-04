a1670ed3d4e47064fde967a5b7fab71cd2d1d9e6
marco76tv <marco.sottana@gmail.com>
2019-06-04 13:58:47 +0200

    .

---
+++ src/Controllers/Admin/RatingController.php
@@ -0,0 +1,12 @@
+<?php
+namespace XRA\Blog\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+//use XRA\Extend\Traits\CrudSimpleTrait as CrudTrait;
+use XRA\Extend\Traits\CrudContainerItemTrait as CrudTrait;
+
+class RatingController extends Controller
+{
+    use CrudTrait;
+    //------------------------------------
+}//end class
